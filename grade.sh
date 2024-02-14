cae75e3c9de561517588a38767769dcfeb9e5957
yunshanlii <156245615+yunshanlii@users.noreply.github.com>
2024-02-14 14:26:19 -0800

    t

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,40 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ]
+then
+    echo "Student submission missing ListExamples.java"
+    echo "Score 0"
+    exit
+fi
+
+javac -cp $CPATH *.java &> compile.txt
+if [ $? -ne 0 ]
+then 
+    echo "Compiliation error"
+    echo "Score 0"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+if [ $? -eq 0 ]; then
+    echo "Score 3/3"
+else 
+    lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+    tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+    failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+    successes=$((tests - failures))
+
+    echo "Your score is $successes / $tests"
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
